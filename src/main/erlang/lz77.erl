ab447b0c0249023c8b17dbba002416eccd5da4ba
Olle Törnström <olle@studiomediatech.com>
2011-01-23 12:59:30 +0100

    Halfway there with the Erlang impl. encoding missing.

--- src/main/erlang/lz77.erl
+++ src/main/erlang/lz77.erl
@@ -1,24 +1,32 @@
- %%% The MIT License
- %%% 
- %%% Copyright (c) 2009 Olle Törnström studiomediatech.com
- %%%
- %%% Permission is hereby granted, free of charge, to any person obtaining a copy
- %%% of this software and associated documentation files (the "Software"), to deal
- %%% in the Software without restriction, including without limitation the rights
- %%% to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- %%% copies of the Software, and to permit persons to whom the Software is
- %%% furnished to do so, subject to the following conditions:
- %%%
- %%% The above copyright notice and this permission notice shall be included in
- %%% all copies or substantial portions of the Software.
- %%% 
- %%% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- %%% IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- %%% FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- %%% AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- %%% LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- %%% OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- %%% THE SOFTWARE.
+%%% The MIT License
+%%% 
+%%% Copyright (c) 2009 Olle Törnström studiomediatech.com
+%%%
+%%% Permission is hereby granted, free of charge, to any person obtaining a copy
+%%% of this software and associated documentation files (the "Software"), to deal
+%%% in the Software without restriction, including without limitation the rights
+%%% to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+%%% copies of the Software, and to permit persons to whom the Software is
+%%% furnished to do so, subject to the following conditions:
+%%%
+%%% The above copyright notice and this permission notice shall be included in
+%%% all copies or substantial portions of the Software.
+%%% 
+%%% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+%%% IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+%%% FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+%%% AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+%%% LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+%%% OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+%%% THE SOFTWARE.
+%%%
+%%% CREDITS: This implementation was inspired by an Erlang LZ77 contribution
+%%%          from https://github.com/psawaya that I received a loooong time ago.
+%%%          I decided to clean up the code "a little" and ended up writing the
+%%%          compression implementation clean new with some bit matching syntax.
+%%%          I'm leaving the author reference though, and a big thanks for the
+%%%          inspiration!
+%%% ----------------------------------------------------------------------------
 -module(lz77).
 
 -author('me@paulsawaya.com').
@@ -26,121 +34,62 @@
 
 -export([decompress/1, compress/1]).
 
+-define(CODE_MASK, 32).
+-define(CODE_BASE, 96).
+-define(MIN_STRING_LENGTH, 5).
+-define(WINDOW_LENGTH, 144).
+
 %% @doc
 %% Decompresses an LZ77 encoded data string.
 %% @end
 decompress(String) ->
-    decode(String, [], 256).
-    %Dico = dict:new(),
-    %decompress(String, Dico, 256, [], [], []).
+    decode(String, []).
 
-decode([], Acc, _WindowLength) ->
-    lists:reverse(Acc);
-decode([$` | Rest], Acc, WindowLength) ->
-    decode_rest(Rest, Acc, WindowLength);
-decode([Char | Rest], Acc, WindowLength) ->
-    NextAcc = [Char | Acc],
-    decode(Rest, NextAcc, WindowLength).
-
-decode_rest([$` | Rest], Acc, WindowLength) ->
-    decode(Rest, [$` | Acc], WindowLength);
-decode_rest([Code | Rest], Acc, WindowLength) ->
-    decode(Rest, [Code | Acc], WindowLength).
-
-%% decompress([], _, _, _, _, Result) ->
-%%     lists:flatten(Result);
-%% decompress([Code | String], Dico, NbChar, Buffer, Chain, Result) ->
-%%     Current = is_ascii_or_in_dict(Dico, Code),
-%%     case Buffer of
-%%         [] ->
-%%             NewBuffer = listify(Current), 
-%%             NewResult = Result ++ listify(Current),
-%%             decompress(String, Dico, NbChar, NewBuffer, Chain, NewResult);
-%%         _ ->
-%%             if
-%%                 Code =< 255 ->
-%%                     NewResult = Result ++ listify(Current),
-%%                     NewChain = Buffer ++ listify(Current),
-
-%%                     NewDico = dict:append(NbChar, NewChain, Dico),
-%%                     NewBuffer = listify(Current),
-%%                     decompress(String, NewDico, NbChar+1, NewBuffer, NewChain, NewResult);
-
-%%                 true ->
-%%                     if 
-%%                         Current == [] ->
-%%                             NewChain = Buffer ++ listify(nth_or_empty_list(1, Buffer));
-
-%%                         true ->
-%%                             NewChain = listify(Current)
-%%                     end,
-
-%%                     NewResult = Result ++ listify(NewChain),
-
-%%                     NewDico = dict:append(NbChar, Buffer ++ listify(nth_or_empty_list(1, NewChain)),  Dico),
-%%                     NewBuffer = NewChain,
-
-%%                     decompress(String, NewDico, NbChar+1, NewBuffer, NewChain, NewResult)
-%% 	    end
-%%     end.
-
+%% @doc
+%% Compresses string data using the LZ77 algorithm.
+%% @end
 compress(String) ->
-    Dico = dict:new(),
-    compress(String ++ [0], Dico, [], 256, []).
-    
-compress([], _, _, _, Result) ->
-    Result;
+    encode(String, []).
 
-compress([Current|String], Dico, Buffer, NbChar, Result) ->
-    case is_ascii_or_in_dict(Dico, Buffer ++ [Current]) of
-        [] ->
-            AddToRes = is_ascii_or_in_dict(Dico, Buffer),
-            
-            if 
-                AddToRes == [] -> 
-                    throw(notFoundInBuffer);
-                true ->
-                    NewResult = Result ++ listify(AddToRes),
-                    NewDico = dict:append(Buffer ++ [Current], NbChar, Dico),
-                    NewNbChar = NbChar + 1,
-                    NewBuffer = [Current],
-                    compress(String, NewDico, NewBuffer, NewNbChar, NewResult)
-            
-            end;
-        
-        _ ->
-            compress(String, Dico, Buffer ++ [Current], NbChar, Result)
-    end.
-          
-nth_or_empty_list(N, List) ->
-    if
-        length(List) >= N ->
-            lists:nth(N, List);
-            
-        true ->
-            []
-    end.
+%% ----------------------------------------------------------------------------
+%% INTERNAL FUNCTIONS
+%% ----------------------------------------------------------------------------
 
-listify(Thing) when is_list(Thing) ->
-    Thing;
+decode([], Acc) ->
+    lists:reverse(Acc);
+decode([$` | Rest], Acc) ->
+    decode_compression(list_to_binary(Rest), Acc);
+decode([Char | Rest], Acc) ->    
+    decode(Rest, [Char | Acc]).
 
-listify(Thing) ->
-    [Thing].
-    
-is_ascii_or_in_dict(Dico, [Lookup]) ->
-    is_ascii_or_in_dict(Dico, Lookup);
+decode_compression(<<$`, Rest/binary>>, Acc) ->
+    decode(binary_to_list(Rest), [$` | Acc]);
+decode_compression(<<B1:8/integer, B2:8/integer, B3:8/integer, Rest/binary>>, Acc) ->
+    {Distance, Length} = decode_compression_bytes(B1, B2, B3),
+    Source = lists:reverse(Acc),
+    Start = length(Source) - Distance - Length,
+    BackRef = lists:reverse(lists:sublist(Source, Start + 1, Length)),
+    NextAcc = lists:flatten([BackRef | Acc]),
+    NextRest = binary_to_list(Rest),
+    decode(NextRest, NextAcc).
 
-is_ascii_or_in_dict(Dico, Lookup) ->
-    case dict:find(Lookup, Dico) of
-        error ->
-            if
-                ((Lookup >= 0) and (Lookup =< 256)) ->
-						%If lookup is an ASCII char, just return it without storing them in the Dico
-                    Lookup;
-                true ->
-                    []
-            end;
+decode_compression_bytes(PMSB, PLSB, LB) ->
+    Position = decode_position(PMSB, PLSB),
+    Length = decode_length(LB),
+    {Position, Length}.
 
-        {ok, [Value]} ->
-	    Value
-    end.
+decode_position(PositionMSByte, PositionLSByte) ->
+    MSValue = (PositionMSByte bxor ?CODE_MASK) * ?CODE_BASE,
+    LSValue = (PositionLSByte bxor ?CODE_MASK),
+    MSValue + LSValue.
+
+decode_length(LengthByte) ->
+    (LengthByte bxor ?CODE_MASK) + ?MIN_STRING_LENGTH.
+
+
+encode([], Acc) ->
+    lists:reverse(Acc);
+encode([Char | Rest], Acc) ->
+    % TODO: Implement encoding :o
+    encode(Rest, [Char | Acc]).
+
--- src/test/erlang/lz77_test.erl
+++ src/test/erlang/lz77_test.erl
@@ -5,8 +5,8 @@
 -define(DECODED, "ababassbabasbabbbaababassbababsbasbasbbabbbababababaaaaabbbab").
 -define(ENCODED, "ababassbabasbabbba` '&bs` 1 sb` 4!` . abaaaa` *!").
 
-%%compress_test() ->
-%%    ?assertEqual(?ENCODED, lz77:compress(?DECODED)).
+compress_test() ->
+    ?assertEqual(?ENCODED, lz77:compress(?DECODED)).
 
 decompress_test() ->
     ?assertEqual(?DECODED, lz77:decompress(?ENCODED)).
